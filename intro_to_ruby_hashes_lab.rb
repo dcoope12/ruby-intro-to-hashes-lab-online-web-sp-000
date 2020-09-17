d9f37ab5fdb9d1e8c01f756017c6b721a5fd8b44
Devin Cooper <dcoope12@asu.edu>
2020-09-17 02:11:21 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,7 +19,7 @@ monopoly = {:railroads => {
 	:names => {},
 	:rent_in_dollars => {}
 	}}
-	
+
 end
 
 def monopoly_with_third_tier
@@ -37,7 +37,7 @@ def monopoly_with_third_tier
 	    :three_pieces_owned => 100,
 	    :four_pieces_owned => 200}
 		}}
-		
+
 end
 
 def monopoly_with_fourth_tier
